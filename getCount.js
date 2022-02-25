e14abfc8295226888cd540eb0fdfa79f3c03b0c9
Caleb Matteis <matteismusic@gmail.com>
2022-02-25 06:09:38 -0500

    commit

---
+++ getCount.js
@@ -0,0 +1,25 @@
+/*
+Description:
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+
+*/
+
+const getCount = (str) => {
+  var vowelsCount = 0;
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  for (let i in str) {
+    for (let j in vowels) {
+      if (str[i] === vowels[j]) {
+        vowelsCount++;
+      }
+    }
+  }
+
+  return vowelsCount;
+};
