aeafcaca38341393156e19fe80146d39e6f8f53c
Caleb Matteis <matteismusic@gmail.com>
2022-03-05 07:12:11 -0500

    commit

--- js/getGrades.js
+++ js/getGrades.js
@@ -1,15 +1,16 @@
 document.querySelector("#myButton").addEventListener("click", grades);
 
-function grades() {
+function grades(e) {
+  e.preventDefault();
   var sum;
   var total;
   var avg;
   var fname = document.getElementById("first-name").value;
   var lname = document.getElementById("last-name").value;
-  var gradeOne = document.getElementById("g1").value;
-  var gradeTwo = document.getElementById("g2").value;
-  var gradeThree = document.getElementById("g3").value;
-  var gradeFour = document.getElementById("g4").value;
+  var gradeOne = parseInt(document.getElementById("g1").value);
+  var gradeTwo = parseInt(document.getElementById("g2").value);
+  var gradeThree = parseInt(document.getElementById("g3").value);
+  var gradeFour = parseInt(document.getElementById("g4").value);
 
   sum = gradeOne + gradeTwo + gradeThree + gradeFour;
   avg = sum / 4;
@@ -26,4 +27,5 @@ function grades() {
   } else {
     document.querySelector("#list").innerText = `${fname} ${lname} mark is F`;
   }
+  console.log(total);
 }
